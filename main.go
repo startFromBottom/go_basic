4d2e76c29c591da54605c86ca86cf16621337d60
startFromBottom <uhh0701@gmail.com>
2020-02-10 01:01:51 +0900

    add mydict.go

--- main.go
+++ main.go
@@ -2,20 +2,57 @@ package main
 
 import (
 	"fmt"
-	"github.com/startFromBottom/learngo/accounts"
 	// "log"
+	// "github.com/startFromBottom/learngo/accounts"
+	"github.com/startFromBottom/learngo/dict"
+	
 )
 
 
 func main() {
-	account := accounts.NewAccount("nico")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
-	err := account.Withdraw(20)
-	if err != nil {
-		// log.Fatalln(err) // kill process
-		fmt.Println(err)
-	}
-	fmt.Println(account)
+	
+	// // part 1 account
+	// account := accounts.NewAccount("nico")
+	// account.Deposit(10)
+	// fmt.Println(account.Balance())
+	// err := account.Withdraw(20)
+	// if err != nil {
+	// 	// log.Fatalln(err) // kill process
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(account)
+
+	// // part2 account
+	// dictionary := mydict.Dictionary{"first": "First word"}
+	// word := "hello"
+	// definition := "Greeting"
+
+	// err := dictionary.Add(word, definition)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+
+	// hello, _ := dictionary.Search(word)
+	// fmt.Println("found", word, "definition : ", hello)
+	// err2 := dictionary.Add(word, definition)
+	// if err2 != nil {
+	// 	fmt.Println(err2)
+	// }
+	
+	// // update
+	// errUpdate := dictionary.Update(word, "alladin")
+	// if errUpdate != nil {
+	// 	fmt.Println(errUpdate)
+	// }
+	// updatedWord, _ := dictionary.Search(word)
+	// fmt.Println(updatedWord)
+
+	// //delete
+	// tempWord := "asdfsdfsdfasdf"
+	// delErr := dictionary.Delete(tempWord)
+	// if delErr != nil {
+	// 	fmt.Println(delErr)
+	// }
+
 }
 
